c7f9ef0bb6e75e0b355a66a3fa7ef2e76366d38d
Thomas Amsler <tamsler@gmail.com>
2015-03-29 16:55:07 -0700

    Serving index.html from node.js

--- src/server.js
+++ src/server.js
@@ -1,39 +1,14 @@
+var http = require('http');
+var fs = require('fs');
 var rest = require('restler');
 var MongoClient = require('mongodb').MongoClient;
-var Server = require('mongodb').Server;
 
-var db;
-var DB_USER = process.env.SPARK_DB_USER;
-var DB_PW = process.evn.SPARK_DB_PW;
-var SP_URL = process.env.SPARK_URL;
-var SP_TOKEN = process.env.SPARK_TOKEN;
+var SP_DB = process.env.SP_DB;
+var SP_URL = process.env.SP_URL;
+var SP_TOKEN = process.env.SP_TOKEN;
+var SP_DB_KEY = process.env.SP_DB_KEY;
 
-var mongoClient = new MongoClient(new Server('ds055709.mongolab.com', 55709), { native_parser: false });
 var tempCollection;
-
-mongoClient.open(function(err, mongoClient) {
-
-    if(err) {
-
-        console.log("DEBUG: err : ", err.message);
-    }
-
-    db = mongoClient.db("spark");
-
-    db.authenticate(DB_USER, DB_PW, function(err, result) {
-
-        if(err) {
-
-            console.log("DEBUG: err 2 : ", err.message);
-        }
-        else {
-
-            tempCollection = db.collection("temp");
-
-            setInterval(getData, 60000);
-        }
-    });
-});
 
 function getData() {
 
@@ -41,7 +16,7 @@ function getData() {
 
         if(data && data.result && data.coreInfo && data.coreInfo.last_heard) {
 
-        tempCollection.insert({ "t" : data.result, "d" : new Date(data.coreInfo.last_heard) }, function(err, result) {
+            tempCollection.insert({ "t" : data.result, "d" : new Date(data.coreInfo.last_heard) }, function(err, result) {
 
                 if(err) {
 
@@ -55,4 +30,61 @@ function getData() {
         }
     });
 }
+
+MongoClient.connect(SP_DB, function(err, db) {
+
+    if(err) {
+
+        console.log("DEBUG: err : ", err.message);
+    }
+    else {
+
+        tempCollection = db.collection("temp");
+
+        setInterval(getData, 60000);
+
+        fs.readFile('./index.html', function (err, html) {
+
+            if (err) {
+
+                throw err;
+            }
+
+            http.createServer(function(request, response) {
+
+                if("/" === request.url) {
+
+                    response.writeHeader(200, {"Content-Type": "text/html"});
+                    response.end(html);
+                }
+                else if(request.url.indexOf("/data") === 0) {
+
+                    var date = new Date();
+                    date.setHours(date.getHours() - 16);
+                    var url = 'https://api.mongolab.com/api/1/databases/spark/collections/temp?q={ "d" : { "$gte" : { "$date" : "' + date.toISOString() +' " } } }&apiKey=' + SP_DB_KEY;
+
+                    rest.get(url).on('complete', function(data) {
+
+                        if(data) {
+
+                            response.writeHeader(200, {"Content-Type": "application/json"});
+                            response.end(JSON.stringify(data));
+                        }
+                        else {
+
+                            response.writeHeader(500, {"Content-Type": "application/json"});
+                            response.end("[]");
+                        }
+                    });
+                }
+                else {
+
+                    response.writeHeader(200, {"Content-Type": "text/plain"});
+                    response.end("Hello");
+                }
+            }).listen(8080);
+        });
+    }
+});
+
 
